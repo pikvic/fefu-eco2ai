7857c0e368f6d3ecb30052ee982cf564b97bf1a5
pikvic <truepikvic@gmail.com>
2022-12-14 10:25:34 +1000

    Update code

--- fefu-eco2ai.py
+++ fefu-eco2ai.py
@@ -6,48 +6,52 @@ import platform
 from time import sleep
 import time
 
-def make_dict(attr_dict):
+def make_report_dict(attr_dict):
     return {
-        "project_name": attr_dict["project_name"],
-        "epoch": attr_dict["epoch"],
-        "start_time": attr_dict["start_time"],
-        "duration(s)": attr_dict["duration(s)"],
-        "power_consumption(kWh)": attr_dict["power_consumption(kWh)"],
-        "CO2_emissions(kg)": attr_dict["CO2_emissions(kg)"],
-        "CPU_name": attr_dict["CPU_name"],
-        "GPU_name": attr_dict["GPU_name"],
-        "OS": attr_dict["OS"]
+        "power_consumption": attr_dict["power_consumption(kWh)"][0],
+        "co2_emissions": attr_dict["CO2_emissions(kg)"][0]
+    }
+
+def make_register_dict(attr_dict, room):
+    return {
+        "name": attr_dict["project_name"][0],
+        "cpu_name": attr_dict["CPU_name"][0],
+        "gpu_name": attr_dict["GPU_name"][0],
+        "os": attr_dict["OS"][0],
+        "description": attr_dict["experiment_description"][0],
+        "room": room
     }
 
 def handler(sig, frame):
     tracker.stop()
     exit(1)
 
-
 signal.signal(signal.SIGINT, handler)
 
 url = "http://localhost:8000"
 MEASURE_PERIOD = 10
+MACHINE_NAME = platform.node()
+DESCRIPTION = "Test for eco2ai" 
+ROOM = "G464"
 
 tracker = eco2ai.Tracker(
-    project_name=platform.node(),
-    experiment_description="Test",
-    measure_period=10,
+    project_name=MACHINE_NAME,
+    experiment_description=DESCRIPTION,
+    measure_period=MEASURE_PERIOD,
     alpha_2_code="RU",
     region="Primorskiy Kray",
     cpu_processes="all"
     )
 
-response = requests.post(f'{url}/v1/machine/', json={"name": platform.node()})
+tracker.start()
+attr_dict = tracker._construct_attributes_dict()
+register_dict = make_register_dict(attr_dict, ROOM)
+print(register_dict)
+response = requests.post(f'{url}/v1/machine/', json=register_dict)
 
 if response.ok:
     machine_id = response.json()["id"]
-else:
-    response = requests.get(f'{url}/v1/machine/{platform.node()}')
-    machine_id = response.json()["id"]
 
-tracker.start()
-print(tracker._construct_attributes_dict())
 prev = tracker.consumption()
 emission_level = tracker.emission_level()
 prev_time = time.time()
@@ -61,9 +65,9 @@ while True:
     prev = tracker.consumption()
     attr_dict["power_consumption(kWh)"] = [consumption]
     attr_dict["CO2_emissions(kg)"] = [consumption * emission_level / 1000]
-    send_dict = make_dict(attr_dict)
+    send_dict = make_report_dict(attr_dict)
     print(send_dict)
-    request = requests.post(f'{url}/v1/report/', json=send_dict)
+    request = requests.post(f'{url}/v1/report/{MACHINE_NAME}/', json=send_dict)
     
     #print("Consumption:", tracker.emission_level())
     #print(tracker._construct_attributes_dict())
