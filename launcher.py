13c3a9c47cba19a5f3dc6b683d5678ba8815d997
pikvic <truepikvic@gmail.com>
2023-02-13 01:18:51 +1000

    Update test

--- launcher.py
+++ launcher.py
@@ -1,27 +1,37 @@
 import requests
 from pathlib import Path
-from worker import work
+import worker
+from importlib import reload
+
+
+version_raw_url = 'https://raw.githubusercontent.com/pikvic/fefu-eco2ai/main/version.txt'
+version_file = Path() / 'version.txt'
+worker_file = Path() / 'worker.py'
+worker_raw_url = 'https://raw.githubusercontent.com/pikvic/fefu-eco2ai/main/worker.py'
 
 def check_updates():
-    url = 'https://raw.githubusercontent.com/pikvic/fefu-eco2ai/main/version.txt'
-    file = Path() / 'version.txt'
-    local_version = file.read_text()
-    print("Local version:", local_version)
-    response = requests.get(url)
-    print("Remote version:", response.raw)
-    
-
+    local_version = version_file.read_text()
+    remote_version = requests.get(version_raw_url).text
+    print("Local version:", local_version, "Remote Version:", remote_version)
+    if local_version != remote_version:
+        return True
+    return False
 
 def update():
     print("Updating...")
-
+    local_version = version_file.read_text()
+    remote_version = requests.get(version_raw_url).text
+    worker_file.write_text(requests.get(worker_raw_url).text)
+    reload(worker)
+    version_file.write_text(remote_version)
     print("Update Finished")
 
 def start():
-    for i in work():
-        print(i)
-        if i["i"] == 10:
-            break
+    for i, v in enumerate(worker.work()):
+        print(v)
+        if i % 5 == 0:
+            if check_updates():
+                break
     return True
 
 if __name__ == "__main__":
@@ -29,5 +39,4 @@ if __name__ == "__main__":
         print("Starting worker...")
         needsUpdate = start()
         if needsUpdate:
-            check_updates()
             update()
--- worker.py
+++ worker.py
@@ -4,7 +4,7 @@ import time
 def work():
     i = 0
     while True:
-        print('start')
+        print('start1')
         time.sleep(1)
         i = i + 1
         yield {'i': i}
